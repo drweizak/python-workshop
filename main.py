94f595735f17c0d26061d91242c157ea67a5b323
Andre Ferreira <andreferreira.coder@gmail.com>
2021-09-19 20:58:31 +0200

    quizz v10

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import pygame
 
+# Definir variaveis
+tamanho_ecra = (768, 768)
+titulo = "Quizz"
+cor_azul_cueca = (153, 204, 255)
+cor_vermelho_cueca = (203,66,84)
+
 # Criar um ecra
-ecra = pygame.display.set_mode((768, 768))
+ecra = pygame.display.set_mode(tamanho_ecra)
 
 # Definir um titulo da janela
-pygame.display.set_caption("Quizz")
+pygame.display.set_caption(titulo)
 
 # Criar um relogio
 relogio = pygame.time.Clock()
 
-# Definir variaveis
+# Definir Fontes
+pygame.font.init()
+titulo_fonte = pygame.font.SysFont("arial", 80, bold=True, italic=False)
 
 # Iniciar ciclo
-
 while True:
 
-    # Ler todos os eventos
+    # Definir cor de fundo
+    ecra.fill(cor_azul_cueca)
+
+    # Definir texto
+    textoTitulo = titulo_fonte.render(titulo, True, cor_vermelho_cueca)
+
+    # Obter Centro do ecra
+    centro_ecra = ecra.get_rect().center
+
+    # Inserir text
+    ecra.blit(textoTitulo, (textoTitulo.get_rect(center = centro_ecra)[0], 150))
+    
+    # Ler todos os evento
     for evento in pygame.event.get():
 
         # Se evento for do tipo QUIT
